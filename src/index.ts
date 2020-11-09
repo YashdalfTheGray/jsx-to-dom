9b30b1a7dc01c42cf15a9eb20fb7539c716dbaf3
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-11-09 01:26:55 -0800

    added some helper functions to the file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export function createElement(
+export default function createElement(
   tag: string,
   props: object = {},
   ...children: HTMLElement[]
@@ -6,12 +6,20 @@ export function createElement(
   const element = document.createElement(tag);
 
   Object.entries(props || {}).forEach(([key, value]) => {
-    if (key.startsWith('on') && key.toLowerCase() in window) {
-      element.addEventListener(key.split('n')[1].toLowerCase(), value);
+    if (isEventHandler(key)) {
+      element.addEventListener(extractEventName(key), value);
     } else {
       element.setAttribute(key, value.toString());
     }
   });
 
   return element;
+}
+
+export function isEventHandler(propName: string) {
+  return propName.startsWith('on') && propName.toLowerCase() in window;
+}
+
+export function extractEventName(propName: string) {
+  return propName.split('n')[1].toLowerCase();
 }
