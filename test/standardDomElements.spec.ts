8a2abaf57d58fb3979c5485bf614282f1b624341
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-11-15 00:55:27 -0800

    reformat the test code to not be indented

--- test/standardDomElements.spec.ts
+++ test/standardDomElements.spec.ts
@@ -3,20 +3,20 @@ import * as babelCore from '@babel/core';
 import { javascriptConfig } from './babelPresets';
 
 const testCode = `
-  import createElement from '../src/index';
+import createElement from '../src/index';
 
-  class StandardDomComponent {
-    render() {
-      return (
-        <div
-          style="display: flex;"
-          onClick={() => console.log('element was clicked')}>
-          <h1>This is a test</h1>
-        </div>
-      )
-    }
+class StandardDomComponent {
+  render() {
+    return (
+      <div
+        style="display: flex;"
+        onClick={() => console.log('element was clicked')}>
+        <h1>This is a test</h1>
+      </div>
+    )
   }
-  `;
+}
+`;
 
 babelCore.transform(testCode, javascriptConfig, (err, result) => {
   if (!err) {
