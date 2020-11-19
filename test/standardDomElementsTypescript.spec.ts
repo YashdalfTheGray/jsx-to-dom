dfa92690f3b572d754868fc87324a341c5725349
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-11-18 19:35:51 -0800

    added a test that handles typescript too

---
+++ test/standardDomElementsTypescript.spec.ts
@@ -0,0 +1,60 @@
+import * as babelCore from '@babel/core';
+import * as chalk from 'chalk';
+import * as diff from 'diff';
+
+import { typescriptConfig } from './babelPresets';
+import { printChanges } from './utils';
+
+const testCode = `
+import createElement from '../src/index';
+
+class StandardDomComponent {
+  public constructor(private componentName: string) {}
+
+  public render() {
+    return (
+      <div
+        style="display: flex;"
+        onClick={() => console.log('element was clicked')}>
+        <h1>This is a test</h1>
+      </div>
+    )
+  }
+}
+`;
+
+const expected = `import createElement from '../src/index';
+
+class StandardDomComponent {
+  constructor(componentName) {
+    this.componentName = componentName;
+  }
+
+  render() {
+    return createElement("div", {
+      style: "display: flex;",
+      onClick: () => console.log('element was clicked')
+    }, createElement("h1", null, "This is a test"));
+  }
+
+}`;
+
+babelCore.transform(testCode, typescriptConfig, (err, result) => {
+  if (!err) {
+    const changes = diff.diffLines(result?.code || '', expected);
+
+    if (changes.length > 1) {
+      console.log(
+        `Test: standard DOM elements | result: ${chalk.red('mismatch')}`
+      );
+      printChanges(changes);
+    } else {
+      console.log(
+        `Test: standard DOM elements | result: ${chalk.green('match')}`
+      );
+    }
+  } else {
+    console.log('There was an error compiling the test code.');
+    console.error(err);
+  }
+});
