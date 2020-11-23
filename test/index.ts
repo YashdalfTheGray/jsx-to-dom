0ea03f6618a340d0e71f9b3a43dc496ac9a5287f
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-11-23 03:47:39 -0500

    added both the fragment tests to the index

--- test/index.ts
+++ test/index.ts
@@ -1,11 +1,17 @@
 import standardDomJs from './standardDomElementsJavascript.spec';
 import standardDomTs from './standardDomElementsTypescript.spec';
+
 import functionalComponentJs from './functionalComponentJavascript.spec';
 import functionalComponentTs from './functionalComponentTypescript.spec';
+
+import fragmentJs from './fragmentJavascript.spec';
+import fragmentTs from './fragmentTypescript.spec';
 
 [
   standardDomJs,
   standardDomTs,
   functionalComponentJs,
   functionalComponentTs,
+  fragmentJs,
+  fragmentTs,
 ].forEach((t) => t.run());
